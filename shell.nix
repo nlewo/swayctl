6bcbe147907c54e26536a0fd4f207e565dada01f
Antoine Eiche <lewo@abesis.fr>
2020-02-14 08:58:32 +0100

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{
+  pkgs ? import (import ./nix/sources.nix).nixpkgs {}
+}:
+
+let
+  updateCrateDeps = pkgs.writeScriptBin "update-crate-deps" ''
+    #!/bin/sh
+    # We need recent patches due to the crate renaming feature
+    nix run -f ${(import ./nix/sources.nix).crate2nix} -c crate2nix generate -n "<nixpkgs>" -f ./Cargo.toml -o Cargo.nix
+  '';
+in
+  pkgs.mkShell {
+    buildInputs = with pkgs; [
+      updateCrateDeps
+      pkg-config
+      rustc
+      cargo
+    ];
+  }
